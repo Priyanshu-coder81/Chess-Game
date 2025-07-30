b6f10ffb4d4826f1143faafe7bd0d37684490428
Priyanshu Verma <vpriyanshu864@gmail.com>
2025-07-30 11:23:05 +0530

    Remove UserContext and associated guest session management utilities
    - Deleted UserContext component responsible for managing user state and guest session creation.
    - Updated useSocket hook to remove guest session handling, streamlining socket connection management.

--- frontend/src/hooks/useSocket.js
+++ frontend/src/hooks/useSocket.js
@@ -1,21 +1,14 @@
 import { useEffect, useState } from "react";
 import { WS_URL } from "../constant";
 import { io } from "socket.io-client";
-import { getOrCreateGuestSession, getSavedGameSession } from "../utils/generateGuestId";
 
 export const useSocket = () => {
   const [socket, setSocket] = useState(null);
 
-  const guestId = getOrCreateGuestSession();
-const savedGameId = getSavedGameSession();
-
   useEffect(() => {
     const socketInstance = io(WS_URL, {
       transports: ["websocket"],
-      query: {
-        guestId,
-        gameId: savedGameId || "",
-      },
+      
     });
 
     setSocket(socketInstance);
